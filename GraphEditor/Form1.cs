f973c39470a0e88b4e6089c24a7c46e31b509c34
Ervim_liiv <opilane@HP-CZC6297B8P>
2022-02-09 15:28:55 +0200

    Add project files.

---
+++ GraphEditor/Form1.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Drawing.Imaging;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace GraphEditor
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void newToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            if (picDrawingSurface.Image == null)
+            {
+                Bitmap pic = new Bitmap(737, 430);
+                picDrawingSurface.Image = pic;
+            }
+            if (picDrawingSurface.Image != null)
+            {
+                var result = MessageBox.Show("Do you want to save an image after create a new file?", "Warning", MessageBoxButtons.YesNoCancel);
+                switch (result)
+                {
+                    case DialogResult.No: break;
+                    case DialogResult.Yes: saveToolStripMenuItem_Click(sender, e); break;
+                    case DialogResult.Cancel: return;
+                }
+            }
+        }
+        private void saveToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            SaveFileDialog SaveDlg = new SaveFileDialog();
+            SaveDlg.Filter = "JPEG Image|*.jpg|Bitmap Image|*.bmp|GIF Image|*.gif|PNG Image|*.png";
+            SaveDlg.Title = "Save an image file";
+            SaveDlg.FilterIndex = 4;
+            SaveDlg.ShowDialog();
+            if (SaveDlg.FileName != "")
+            {
+                System.IO.FileStream fs = (System.IO.FileStream)SaveDlg.OpenFile();
+                switch (SaveDlg.FilterIndex)
+                {
+                    case 1:
+                        this.picDrawingSurface.Image.Save(fs, ImageFormat.Jpeg);
+                        break;
+                    case 2:
+                        this.picDrawingSurface.Image.Save(fs, ImageFormat.Bmp);
+                        break;
+                    case 3:
+                        this.picDrawingSurface.Image.Save(fs, ImageFormat.Gif);
+                        break;
+                    case 4:
+                        this.picDrawingSurface.Image.Save(fs, ImageFormat.Png);
+                        break;
+                }
+                fs.Close();
+            }
+        }
+
+
+        private void picDrawingSurface_MouseDown(object sender, MouseEventArgs e)
+        {
+            if (picDrawingSurface.Image == null)
+            {
+                MessageBox.Show("Create a new file first!");
+                return;
+            }
+        }
+    }
+}
